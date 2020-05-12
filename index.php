742c299eaeac08e2b964346c1ffcf1aec1bdc967
viocuong <viocuong@users.noreply.github.com>
2020-05-12 23:50:07 +0700

    up

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    $HOST='localhost';
-    //$HOST='3.22.79.240';
+    //$HOST='localhost';
+    $HOST='3.22.79.240';
     require_once './mvc/application.php';
     $a=new App();
 ?>
